33ce14afebe9c10f329d23177423f5a9c3b853d2
Shyim <s.sayakci@shopware.com>
2022-10-06 21:17:58 +0200

    auto install required npm packages of admin

--- shopware/administration/6.4/bin/watch-administration.sh
+++ shopware/administration/6.4/bin/watch-administration.sh
@@ -5,12 +5,22 @@ CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 export PROJECT_ROOT="${PROJECT_ROOT:-"$(dirname "$CWD")"}"
 export ENV_FILE=${ENV_FILE:-"${PROJECT_ROOT}/.env"}
 
-source "${ENV_FILE}"
+LOAD_DOTENV=${LOAD_DOTENV:-"1"}
+
+if [[ "$LOAD_DOTENV" == "1" ]]; then
+    source "${ENV_FILE}"
+fi
+
 export HOST=${HOST:-"localhost"}
 export ESLINT_DISABLE
 export PORT
 export APP_URL
 
+bin/console bundle:dump
 bin/console feature:dump || true
+
+if [ ! -d vendor/shopware/administration/Resources/app/administration/node_modules ]; then
+    npm install --prefix vendor/shopware/administration/Resources/app/administration/
+fi
 
 npm run --prefix vendor/shopware/administration/Resources/app/administration/ dev
