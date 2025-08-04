fa4445f53488275e2b4922794b38b7d273a8bd92
Patrick Weyck <p.weyck@shopware.com>
2025-08-04 09:35:03 +0200

    fix: disable signing for boost177 to fix devenv build on mac

--- frosh/devenv-meta/0.3/root/devenv.nix
+++ frosh/devenv-meta/0.3/root/devenv.nix
@@ -6,6 +6,19 @@
     pkgs.gnupatch
   ];
 
+  overlays = [
+    # Each overlay is a function that takes two arguments: final and prev
+    (final: prev: {
+      # Override an existing package
+      boost177 = prev.boost177.overrideAttrs (oldAttrs: {
+        # The postFixup hook is where the signing check runs.
+        # By setting it to an empty string, we tell Nix to skip it.
+        # This is generally safe for development shells.
+        postFixup = "";
+      });
+    })
+  ];
+
   languages.javascript = {
     enable = lib.mkDefault true;
     package = lib.mkDefault pkgs.nodejs_20;
